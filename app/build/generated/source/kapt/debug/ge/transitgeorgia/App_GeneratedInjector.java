11fc1cf763ba1e659c9cb119c35eff537e8fb6b5
George Gigauri <giorgi.gigauri.1@btu.edu.ge>
2024-01-27 15:59:05 +0400

    build.gradle.kts

---
+++ app/build/generated/source/kapt/debug/ge/transitgeorgia/App_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package ge.transitgeorgia;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = App.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface App_GeneratedInjector {
+  void injectApp(App app);
+}
---
+++ app/build/generated/source/kapt/debug/hilt_aggregated_deps/_ge_transitgeorgia_App_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package hilt_aggregated_deps;
+
+import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedDeps(
+    components = "dagger.hilt.components.SingletonComponent",
+    entryPoints = "ge.transitgeorgia.App_GeneratedInjector"
+)
+@Generated("dagger.hilt.processor.internal.aggregateddeps.AggregatedDepsGenerator")
+public class _ge_transitgeorgia_App_GeneratedInjector {
+}
