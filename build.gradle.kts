b23e653b7e4fb47124e82504c2e59304aae3c7a6
george-gigauri <giorgi.gigauri.1@btu.edu.ge>
2024-05-07 14:33:48 +0400

    upgrade agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.2")
+        classpath("com.android.tools.build:gradle:8.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
         classpath("com.google.gms:google-services:4.4.1")
